d4d139ed0e7e9a07918bad0168a1d323f827e601
Awa Melvine <melvineawa@gmail.com>
2019-07-15 19:56:13 +0100

    Modify get accounts model method to take optional id parameter

--- models/Account.js
+++ models/Account.js
@@ -1,8 +1,13 @@
 const db = require('../data/dbConfig');
 
 module.exports = {
-    get() {
-        return db('accounts');
+    get(id = null) {
+        const query = db('accounts as a');
+        if (id) {
+            return query.where('a.id', id).first();
+        } else {
+            return query;
+        }
     },
 
     insert(account) {
