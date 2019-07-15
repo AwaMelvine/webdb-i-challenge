458468aa9f637b855f6af469865370c48fb8cb75
Awa Melvine <melvineawa@gmail.com>
2019-07-15 20:05:25 +0100

    Create endpoint to update account

--- routes/accounts.js
+++ routes/accounts.js
@@ -33,4 +33,14 @@ router.post("/", async (req, res) => {
     }
 });
 
+router.put("/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const account = await Account.update(id, req.body);
+        res.status(200).json({ account });
+    } catch (error) {
+        res.status(500).json({ error: "Unable to update account" })
+    }
+});
+
 module.exports = router;
